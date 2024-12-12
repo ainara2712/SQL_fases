e054a871e9b92b1eb011aaf36e8673afcc7fc5f0
Fedeerich <marcosgongora1000@gmail.com>
2024-12-12 12:37:49 +0100

    Primer dia

---
+++ FASE1.sql
@@ -0,0 +1,69 @@
+/* *****************************************************
+  INSTITUT TIC de Barcelona
+    CFGS Desenvolupament d'aplicacions web
+    Mòdul: 0484 Bases de dades. 
+    AUTORS: Marcos Góngora Carriedo i Ainara Juàrez Moreno
+    DATA: 12/12/24
+****************************************************** */
+
+-- Creació de les taules
+
+DROP TABLE IF EXISTS Companyia;
+
+CREATE TABLE Companyia (
+    nom VARCHAR(40) NOT NULL,
+    IATA CHAR(6) NOT NULL,
+    CODE3 CHAR(6) NOT NULL,
+    ICAO CHAR(6) NOT NULL,
+    pais VARCHAR(40) NOT NULL
+);
+
+DROP TABLE IF EXISTS Avió;
+
+CREATE TABLE Avió (
+    num_serie VARCHAR(30) NOT NULL,
+    tipus VARCHAR(10) NOT NULL,
+    fabricant VARCHAR(20) NOT NULL,
+    any_fabricacio YEAR
+);
+
+DROP TABLE Aeroport;
+
+CREATE TABLE Aeroport (
+    codi CHAR(4) NOT NULL,
+    pais VARCHAR(40) NOT NULL,
+    ciutat VARCHAR(40) NOT NULL,
+    IATA CHAR(4) NOT NULL,
+    nom VARCHAR(55) NOT NULL,
+    any_construccio YEAR
+);
+
+DROP TABLE Mostrador;
+
+CREATE TABLE Mostrador (
+    numero INTEGER 
+)
+
+DROP TABLE Personal;
+
+CREATE TABLE Personal (
+    num_empleat INTEGER,
+    nom VARCHAR(25) NOT NULL,
+    cognom VARCHAR(35) NOT NULL,
+    passaport VARCHAR(20) NOT NULL,
+    sou FLOAT NOT NULL
+)
+
+DROP TABLE Hostessa;
+
+CREATE TABLE Hostessa (
+    
+)
+
+DROP TABLE Hostessa;
+
+CREATE TABLE Hostessa (
+    
+)
+
+

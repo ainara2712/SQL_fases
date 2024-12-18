573c1d2058c17ea8bfd9e562bc793bcb6d1ca76b
Fedeerich <marcosgongora1000@gmail.com>
2024-12-18 17:47:32 +0100

    ACTUALIZACIÓN

--- FASE1.sql
+++ FASE1.sql
@@ -1,8 +1,8 @@
 /* *****************************************************
   INSTITUT TIC de Barcelona
-    CFGS Desenvolupament d'aplicacions web
+    CFGS Desenvolupament d'aplicacions web.
     Mòdul: 0484 Bases de dades. 
-    AUTORS: Marcos Góngora Carriedo i Ainara Juàrez Moreno
+    AUTORS: Marcos Góngora Carriedo i Ainara Juàrez Moreno.
     DATA: 12/12/24
 ****************************************************** */
 
@@ -27,7 +27,7 @@ CREATE TABLE Avió (
     any_fabricacio YEAR
 );
 
-DROP TABLE Aeroport;
+DROP TABLE IF EXISTS Aeroport;
 
 CREATE TABLE Aeroport (
     codi CHAR(4) NOT NULL,
@@ -38,13 +38,13 @@ CREATE TABLE Aeroport (
     any_construccio YEAR
 );
 
-DROP TABLE Mostrador;
+DROP TABLE IF EXISTS Mostrador;
 
 CREATE TABLE Mostrador (
     numero INTEGER 
-)
+);
 
-DROP TABLE Personal;
+DROP TABLE IF EXISTS Personal;
 
 CREATE TABLE Personal (
     num_empleat INTEGER,
@@ -52,42 +52,38 @@ CREATE TABLE Personal (
     cognom VARCHAR(35) NOT NULL,
     passaport VARCHAR(20) NOT NULL,
     sou FLOAT NOT NULL
-)
+);
 
-DROP TABLE Hostessa;
-
-CREATE TABLE Hostessa (
-    
-)
-
-DROP TABLE Hostessa;
+DROP TABLE  IF EXISTS Hostessa;
 
 CREATE TABLE Hostessa (
     
-)
+);
 
-DROP TABLE Pilot;
+DROP TABLE IF EXISTS Pilot;
 
 CREATE TABLE Pilot (
     hores_vol INTEGER (4)
-)
+);
 
-DROP TABLE Passatger;
+DROP TABLE IF EXISTS Passatger;
 
 CREATE TABLE Passatger (
     passaport VARCHAR(20) NOT NULL,
     nom VARCHAR(20) NOT NULL,
     cognom VARCHAR(50) NOT NULL,
+    adreca VARCHAR(70),
+    telefon CHAR(9),
     email VARCHAR(40) NOT NULL,
     data_naixement YEAR,
     genere CHAR(1)
-)
+);
 
-DROP TABLE Vol;
+DROP TABLE IF EXISTS Vol;
 
 CREATE TABLE Vol (
     codi VARCHAR(9) NOT NULL,
     data YEAR NOT NULL,
     durada MINUTS NOT NULL,
     descripcio VARCHAR()
-)
+);
